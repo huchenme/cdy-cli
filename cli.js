6d599861ddeb4c83ba287731c7e4a5a85be41ba9
Hu Chen <chu@zendesk.com>
2020-05-10 15:15:44 +0800

    initial commit

--- cli.js
+++ cli.js
@@ -1,6 +1,30 @@
 #!/usr/bin/env node
 
 'use strict';
-console.log('hello world2');
+const { exec } = require('child_process');
+const updateNotifier = require('update-notifier');
+const chalk = require('chalk');
+const pkg = require('./package.json');
+
 const args = process.argv.slice(2);
-console.log(args);
+let folder = args[0];
+let command = args.slice(1).join(' ');
+
+updateNotifier({ pkg }).notify();
+
+try {
+  process.chdir(folder);
+  console.log(
+    `Executing ${chalk.blue(command)} in ${chalk.blue(folder)} folder...\n`
+  );
+  exec(command, (error, stdout, stderr) => {
+    if (stdout) {
+      process.stdout.write(stdout);
+    }
+    if (stderr) {
+      process.stderr.write(`❌ ${chalk.red(stderr)}`);
+    }
+  });
+} catch (err) {
+  console.error(`❌ folder ${chalk.red(folder)} does not exists`);
+}
